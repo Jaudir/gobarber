7a6ffcfb574c397a4d7c696af157d2bddd152cca
jaudir <5773247+jaudir@users.noreply.github.com>
2020-01-30 21:19:42 -0300

    feat: Add appointment

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,22 @@
+import Sequelize, { Model } from 'sequelize';
+
+class File extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'avatar_id', as: 'avatar' });
+  }
+}
+export default File;
--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
 import Sequelize from 'sequelize';
 import User from '../app/models/User';
 import File from '../app/models/File';
-
+import Appointment from '../app/models/Appointment';
 import databaseConfig from '../config/database';
 
-const models = [File, User];
+const models = [Appointment, File, User];
 
 class Database {
   constructor() {
